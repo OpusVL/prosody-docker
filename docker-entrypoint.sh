7a784496e1738c3a9f3b8423e9c490d8dc5efd56
Kane Valentine <kane@cute.im>
2018-03-06 16:07:42 +0000

    symlink community modules to prosody plugin path based on environment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+perl -e'system "ln", "-s", "/opt/prosody-modules-available/mod_$_", "/opt/prosody-modules-enabled/mod_$_" for map s/"//gr, split /[,;]\s*/, $ENV{PROSODY_COMM_MODULES}'
+
 exec "$@"
