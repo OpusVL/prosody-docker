398ddc311514e77595fb55efb962ba2a09ca1c9b
Alastair McGowan-Douglas <alastair.douglas@gmail.com>
2018-03-07 18:12:31 +0000

    Don't need this entrypoint

--- docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-perl -e'system "ln", "-s", "/opt/prosody-modules-available/mod_$_", "/opt/prosody-modules-enabled/mod_$_" for map s/"//gr, split /[,;]\s*/, $ENV{PROSODY_COMM_MODULES}'
-
-exec "$@"
